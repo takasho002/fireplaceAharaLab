1d542ac392551174637e83c63bcffe5645b1d7de
Kazushi Ahara <ahara@meiji.ac.jp>
2020-09-09 14:59:06 +0900

    Update agent_RogueCat.py

--- fireplaceAharaLab/agent_RogueCat.py
+++ fireplaceAharaLab/agent_RogueCat.py
@@ -1,3 +1,20 @@
 # agent_RogueCat.py
 
+import random
 
+from utils import ExceptionPlay, Candidate, executeAction, getCandidates
+from agent_Standard import postAction
+
+def printAllRogueCards():
+	    
+	pass
+
+def RogueCatAI(game: Game, option=[], debugLog=False):
+	while True:
+		myCandidate = getCandidates(game)
+		if len(myCandidate)>0:
+			myChoice = random.choice(myCandidate)
+			executeAction(thisgame, myChoice, debugLog=debugLog)
+			postAction(player)
+		else:
+			return
