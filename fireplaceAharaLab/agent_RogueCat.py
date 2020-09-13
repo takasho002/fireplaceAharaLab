b2bb93c81cc339935ac8ff93de867ccc94d5ce17
Kazushi Ahara <ahara@meiji.ac.jp>
2020-09-13 17:49:51 +0900

    Update agent_RogueCat.py

--- fireplaceAharaLab/agent_RogueCat.py
+++ fireplaceAharaLab/agent_RogueCat.py
@@ -292,7 +292,7 @@ def RogueCat_Condition(game, card, target):
 				cond1.append(['deathrattle','', 'drawCard'])
 				#踊る剣 : [x]<b>断末魔:</b>相手はカードを1枚引く。
 			elif ID == 'FP1_031':
-				cond1.append(['','haveDeathrattleMinion(game)', ''])
+				cond1.append(['','haveDeathrattle(game)', ''])
 				#バロン・リーヴェンデア : [x]味方のミニオンの__<b>断末魔</b>は2回発動する。
 		elif 'GIL_' in ID:
 			if ID == 'GIL_118':
@@ -600,7 +600,7 @@ def RogueCat_Condition(game, card, target):
 				cond1.append(['battlecry','', ''])
 				#傷を負ったクヴァルディル : <b>雄叫び:</b> このミニオンに3ダメージを与える。
 			elif ID == 'AT_106':
-				cond1.append(['battlecry','heHasDevil(game)', 'silence'])
+				cond1.append(['battlecry','heHasDemon(game)', 'silence'])
 				#光の勇者 : <b>雄叫び:</b> 悪魔1体を<b>沈黙</b>させる。
 			elif ID == 'AT_108':
 				cond1.append(['battlecry','', ''])
@@ -859,7 +859,7 @@ def RogueCat_Condition(game, card, target):
 				cond1.append(['battlecry','', ''])
 				#グライムストリートの運び屋 : <b>雄叫び:</b>自分の手札のランダムなミニオン1体に+1/+1を付与する。
 			elif ID == 'CFM_855':
-				cond1.append(['battlecry','HeHasDeathrattle(game)', 'silence'])
+				cond1.append(['battlecry','heHasDeathrattle(game)', 'silence'])
 				#デファイアスの掃除屋 : [x]<b>雄叫び:</b><b>断末魔</b>を持つミニオン1体を<b>沈黙</b>させる。
 		elif 'CS2_' in ID:
 			if ID == 'CS2_117':
@@ -2609,41 +2609,86 @@ def RogueCat_Condition(game, card, target):
 			cond1.append(['spellDamage','', ''])
 			#強仙師 : ダメージを受けている間は<b>呪文ダメージ+2</b>を得る。
 	if '<b>猛毒</b>' in dscrpt:
-#<b>猛毒</b>
+		#<b>猛毒</b>
 		pass
-	elif '<b>超電磁</b>' in dscrpt:
-#<b>超電磁</b>
+	if '<b>超電磁</b>' in dscrpt:
+		#<b>超電磁</b>
 		pass
-	elif '<b>急襲</b>' in dscrpt:
-#<b>急襲</b>
+	if '<b>急襲</b>' in dscrpt:
+		#<b>急襲</b>
 		pass
-	elif '<b>生命奪取</b>' in dscrpt:
-#<b>生命奪取</b>
+	if '<b>生命奪取</b>' in dscrpt:
+		#<b>生命奪取</b>
 		pass
-	elif '<b>休眠状態</b>' in dscrpt:
-#<b>休眠状態</b>
+	if '<b>休眠状態</b>' in dscrpt:
+		#<b>休眠状態</b>
 		pass
-	elif '<b>発見</b>' in dscrpt:
-#<b>発見</b>
+	if '<b>発見</b>' in dscrpt:
+		#<b>発見</b>
 		pass
 
 ########### 度 #################
 
-	elif '武器を装備する度' in dscrpt:
-#[AT_029][バッカニーア](自分が武器を装備する度、その武器に攻撃力+1を付与する。)
-#[CFM_325][ちんけなバッカニーア]([x]自分のヒーローが武器を装備している場合攻撃力+2を得る。)
-#[GVG_119][ブリングトロン3000](<b>雄叫び:</b> 各プレイヤーはランダムな武器を装備する。)
-		cond1 += 1
-		for card in player.characters:
-			if card.type == CardType.WEAPON:
-				cond1 += 1
-	elif 'ヒーローを攻撃する度' in dscrpt:
-#[AT_032][闇商人]([x]<b>雄叫び:</b>味方に海賊がいる場合___+1/+1を獲得する。)
-#[AT_070][空賊船長クラッグ](<b>突撃ィィィィ</b>味方の海賊1体につきコストが（1）減る。)
-#[NEW1_027][南海の船長](自身を除く味方の海賊は+1/+1を得る。)
-#[TRL_127][大砲連射]([x]ランダムな敵1体に$3ダメージを与える。味方の海賊1体につき1回これを繰り返す。)
-		if target!=None and target.type == CardType.HERO:
-			cond1 += 2
+	if ID=='CFM_325':
+		if haveWeapon(game):
+			cond1.append(['weapon','','help'])
+			#[CFM_325][ちんけなバッカニーア]([x]自分のヒーローが武器を装備している場合攻撃力+2を得る。)
+	elif ID=='NEW1_027':
+		if havePirate(game):
+			cond1.append(['pirate','','help'])
+			#[NEW1_027][南海の船長](自身を除く味方の海賊は+1/+1を得る。)
+	elif ID=='TRL_127':
+		if havePirate(game):
+			cond1.append(['pirate','','damage'])
+		#[TRL_127][大砲連射]([x]ランダムな敵1体に$3ダメージを与える。味方の海賊1体につき1回これを繰り返す。)
+
+
+
+
+############### キーワード #####################	
+	if 'コンボ' in dscrpt:
+		if player.combo:
+			pass
+#[AT_028][シャドーパン騎兵](<b>コンボ:</b> 攻撃力+3を獲得する。)
+#[AT_030][アンダーシティの勇士](<b>コンボ:</b> 1ダメージを与える。)
+#[BOT_576][イカレた化学者]([x]<b>コンボ:</b>味方のミニオン1体に__攻撃力+4を付与する。)
+#[CFM_690][翡翠の手裏剣]([x]__$2ダメージを与える。<b>コンボ:</b> {0}の<b>翡翠のゴーレム</b>を1体召喚する。@[x]__$2ダメージを与える。<b>コンボ:</b><b>翡翠のゴーレム</b>を1体召喚する。)
+#[CFM_693][ガジェッツァンの渡し守]([x]<b>コンボ:</b>味方のミニオン1体を___自分の手札に戻す。)
+#[CS2_073][冷血]([x]ミニオン1体に攻撃力+2を付与する。<b>コンボ:</b> 代わりに攻撃力+4を付与する。)
+#[DAL_415][悪党同盟の悪漢]([x]<b>コンボ:</b>ランダムな<b>悪の手先</b>2体を自分の手札に追加する。)
+#[DRG_031][ネクリウムの薬師]([x]<b>コンボ:</b>自分のデッキから<b>断末魔</b>を持つミニオンを1体引きその<b>断末魔</b>を獲得する。)
+#[EX1_124][腹裂き]([x]$2ダメージを与える。<b>コンボ:</b> 代わりに$4ダメージを与える。)
+#[EX1_131][デファイアスの親方]([x]<b>コンボ:</b>2/1のデファイアスの盗賊を1体召喚する。)
+#[EX1_133][地獄送りの刃]([x]<b>雄叫び:</b>_____1ダメージを与える。__ <b>コンボ:</b> 代わりに________2ダメージを与える。)
+#[EX1_134][SI:7諜報員]([x]<b>コンボ:</b> 2ダメージを与える。)
+#[EX1_137][脳天直撃]([x]敵のヒーローに$2ダメージを与える。<b>コンボ:</b> 次のターン、このカードを自分の手札に戻す。)
+#[EX1_613][エドウィン・ヴァンクリーフ](<b>コンボ:</b>このターン中で先に使用されたカード1枚につき+2/+2を獲得する。)
+#[GIL_557][呪われた漂流者]([x]<b>急襲</b>、<b>断末魔:</b>自分のデッキから<b>コンボ</b>カードを_1枚引く。)
+#[GIL_902][人斬りバッカニーア](<b>コンボ:</b>自分の武器に攻撃力+1を__付与する。)
+#[GVG_022][ティンカーの刃研ぎ油](自分の武器に攻撃力+3を付与する。<b>コンボ:</b> ランダムな味方のミニオン1体に攻撃力+3を付与する。)
+#[GVG_047][サボタージュ]([x]ランダムな敵のミニオン1体を破壊する。<b>コンボ:</b> さらに敵の武器を破壊する。)
+#[ICC_809][疫病科学者]([x]<b>コンボ:</b>味方のミニオン1体に<b>猛毒</b>を付与する。)
+#[ICC_910][略奪の亡霊]([x]<b>コンボ:</b>このターン中で先に使用したカードの枚数に等しいダメージを与える。)
+#[LOOT_211][エルフの吟遊楽人]([x]<b>コンボ:</b>自分のデッキから__ミニオンを2体引く。)
+#[NEW1_005][誘拐魔]([x]<b>コンボ:</b> ミニオン1体を___所有者の手札に戻す。)
+#[OG_070][凶刃の狂信者]([x]<b>コンボ:</b>______+1/+1を獲得する。)
+#[SCH_234][偽善系の二年生]([x]<b>隠れ身</b>、<b>魔法活性:</b><b>コンボ</b>カード1枚を___自分の手札に追加する。)
+#[SCH_350][ワンド泥棒]([x]<b>コンボ:</b>メイジの呪文を1つ<b>発見</b>する。)
+#[SCH_509][頭を冷やせ！]([x]ミニオン1体を<b>凍結</b>させる。<b>コンボ:</b>さらに$3ダメージを与える。)
+#[SCH_521][無理強い]([x]ダメージを受けているミニオン1体を破壊する。<b>コンボ:</b>ミニオン1体を破壊する。)
+#[TRL_092][サメの精霊]([x]1ターンの間、<b>隠れ身</b>。味方のミニオンの<b>雄叫び</b>と<b>コンボ</b>は2回発動する。)
+#[TRL_124][ぶんどり部隊](自分のデッキから海賊を2体引く。<b>コンボ:</b>さらに武器を1つ引く。)
+#[ULD_231][旋風脚流の達人]([x]自分が<b>コンボ</b>カードを手札から使用する度ランダムな<b>コンボ</b>カード1枚を自分の手札に追加する。)
+#[ULD_285][鉤付きシミター](<b>コンボ:</b>攻撃力+2を獲得する。)
+#[UNG_063][カミツキソウ](<b>コンボ:</b> このターン中で先に使用されたカード1枚につき+1/+1を獲得する。)
+#[UNG_064][ヴァイルスパイン・スレイヤー](<b>コンボ:</b>ミニオン1体を破壊する。)
+#[YOD_017][影の彫刻家]([x]<b>コンボ:</b>このターン中に先に使用されたカード1枚につきカードを1枚引く。)
+
+#使わない
+#[BOT_447][結晶術師]([x]<b>雄叫び:</b>自分のヒーローに___5ダメージを与える。_____装甲を5獲得する。_)
+#[TRL_546][凶暴なリクガメ]([x]<b>雄叫び:</b>自分のヒーローに___5ダメージを与える。)
+#[UNG_087][ビタータイド・ヒドラ](このミニオンがダメージを受ける度自分のヒーローに___3ダメージを与える。)
+
 #自分のヒーローがダメージを受ける度
 #ヒーローを攻撃する度
 #武器を装備する度
@@ -2686,142 +2731,7 @@ def RogueCat_Condition(game, card, target):
 #隣接するミニオンがダメージを受ける度
 #自分のヒーローの体力を3以上回復する度
 #自分が<b>コンボ</b>カードを手札から使用する度
-
-
-############# 場合 #########################
-
-	if 'ドラゴン' in dscrpt:
-		if ID == 'AT_017':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[AT_017][トワイライトの守護者]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合、攻撃力+1と<b>挑発</b>を獲得する。)
-		if ID == 'AT_123':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[AT_123][チルモー]([x]<b>挑発</b>、<b>断末魔:</b>自分の手札にドラゴンがいる場合、全てのミニオンに3ダメージを与える。)
-		if ID == 'BRM_029':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[BRM_029][レンド・ブラックハンド]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合<b>レジェンド</b>ミニオン1体を破壊する。)
-		if ID == 'BRM_033':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[BRM_033][ブラックウィングの技術者]([x]<b>雄叫び:</b> 自分の手札にドラゴンがいる場合____+1/+1を獲得する。)
-		if ID == 'BRM_034':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[BRM_034][ブラックウィングの変性者]([x]<b>雄叫び:</b> 自分の手札にドラゴンがいる場合_____3ダメージを与える。)
-		if ID == 'DRG_072':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[DRG_072][スカイフィン](<b>雄叫び:</b>自分の手札にドラゴンがいる場合ランダムなマーロックを2体召喚する。)
-		if ID == 'DRG_081':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[DRG_081][スケイルライダー]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合_____2ダメージを与える。)
-		if ID == 'GIL_526':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[GIL_526][ワームガード]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合攻撃力+1と<b>挑発</b>を獲得する。)
-		if ID == 'KAR_033':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[KAR_033][ブック・ワーム]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合攻撃力3以下の敵の______ミニオン1体を破壊する。_)
-		if ID == 'GIL_601':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[GIL_601][スケイルワーム]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合攻撃力+1と<b>急襲</b>を獲得する。)
-		if ID == 'KAR_062':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[KAR_062][ネザースパイトの歴史家]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合____ドラゴン1体を<b>発見</b>する。)
-		if ID == 'TRL_523':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[TRL_523][ファイアーツリーの呪術医]([x]<b>雄叫び:</b>自分の手札にドラゴンがいる場合_____呪文を1つ<b>発見</b>する。)
-		if ID == 'TRL_569':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[TRL_569][ドッカンドラゴン](<b>雄叫び:</b>自分の手札にドラゴンがいる場合敵のミニオン1体に__7ダメージを与える。)
-		if ID == 'DRG_063':
-			if haveDragon(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[DRG_063][ドラゴンモーの密猟者](<b>雄叫び:</b>相手の陣地にドラゴンがいる場合、+4/+4と<b>急襲</b>を獲得する。)
-		if ID == 'DRG_089':
-			if haveNoDuplicate(game):
-				cond1.append(['dragon','' ,''])
-			pass
-			#[DRG_089][竜の女王アレクストラーザ]([x]<b>雄叫び:</b>_自分のデッキに重複するカードがない場合
-
-
-############### キーワード #####################	
-	if 'コンボ' in dscrpt:
-		if player.combo:
-			pass
-#[AT_028][シャドーパン騎兵](<b>コンボ:</b> 攻撃力+3を獲得する。)
-#[AT_030][アンダーシティの勇士](<b>コンボ:</b> 1ダメージを与える。)
-#[BOT_576][イカレた化学者]([x]<b>コンボ:</b>味方のミニオン1体に__攻撃力+4を付与する。)
-#[CFM_690][翡翠の手裏剣]([x]__$2ダメージを与える。<b>コンボ:</b> {0}の<b>翡翠のゴーレム</b>を1体召喚する。@[x]__$2ダメージを与える。<b>コンボ:</b><b>翡翠のゴーレム</b>を1体召喚する。)
-#[CFM_693][ガジェッツァンの渡し守]([x]<b>コンボ:</b>味方のミニオン1体を___自分の手札に戻す。)
-#[CS2_073][冷血]([x]ミニオン1体に攻撃力+2を付与する。<b>コンボ:</b> 代わりに攻撃力+4を付与する。)
-#[DAL_415][悪党同盟の悪漢]([x]<b>コンボ:</b>ランダムな<b>悪の手先</b>2体を自分の手札に追加する。)
-#[DRG_031][ネクリウムの薬師]([x]<b>コンボ:</b>自分のデッキから<b>断末魔</b>を持つミニオンを1体引きその<b>断末魔</b>を獲得する。)
-#[EX1_124][腹裂き]([x]$2ダメージを与える。<b>コンボ:</b> 代わりに$4ダメージを与える。)
-#[EX1_131][デファイアスの親方]([x]<b>コンボ:</b>2/1のデファイアスの盗賊を1体召喚する。)
-#[EX1_133][地獄送りの刃]([x]<b>雄叫び:</b>_____1ダメージを与える。__ <b>コンボ:</b> 代わりに________2ダメージを与える。)
-#[EX1_134][SI:7諜報員]([x]<b>コンボ:</b> 2ダメージを与える。)
-#[EX1_137][脳天直撃]([x]敵のヒーローに$2ダメージを与える。<b>コンボ:</b> 次のターン、このカードを自分の手札に戻す。)
-#[EX1_613][エドウィン・ヴァンクリーフ](<b>コンボ:</b>このターン中で先に使用されたカード1枚につき+2/+2を獲得する。)
-#[GIL_557][呪われた漂流者]([x]<b>急襲</b>、<b>断末魔:</b>自分のデッキから<b>コンボ</b>カードを_1枚引く。)
-#[GIL_902][人斬りバッカニーア](<b>コンボ:</b>自分の武器に攻撃力+1を__付与する。)
-#[GVG_022][ティンカーの刃研ぎ油](自分の武器に攻撃力+3を付与する。<b>コンボ:</b> ランダムな味方のミニオン1体に攻撃力+3を付与する。)
-#[GVG_047][サボタージュ]([x]ランダムな敵のミニオン1体を破壊する。<b>コンボ:</b> さらに敵の武器を破壊する。)
-#[ICC_809][疫病科学者]([x]<b>コンボ:</b>味方のミニオン1体に<b>猛毒</b>を付与する。)
-#[ICC_910][略奪の亡霊]([x]<b>コンボ:</b>このターン中で先に使用したカードの枚数に等しいダメージを与える。)
-#[LOOT_211][エルフの吟遊楽人]([x]<b>コンボ:</b>自分のデッキから__ミニオンを2体引く。)
-#[NEW1_005][誘拐魔]([x]<b>コンボ:</b> ミニオン1体を___所有者の手札に戻す。)
-#[OG_070][凶刃の狂信者]([x]<b>コンボ:</b>______+1/+1を獲得する。)
-#[SCH_234][偽善系の二年生]([x]<b>隠れ身</b>、<b>魔法活性:</b><b>コンボ</b>カード1枚を___自分の手札に追加する。)
-#[SCH_350][ワンド泥棒]([x]<b>コンボ:</b>メイジの呪文を1つ<b>発見</b>する。)
-#[SCH_509][頭を冷やせ！]([x]ミニオン1体を<b>凍結</b>させる。<b>コンボ:</b>さらに$3ダメージを与える。)
-#[SCH_521][無理強い]([x]ダメージを受けているミニオン1体を破壊する。<b>コンボ:</b>ミニオン1体を破壊する。)
-#[TRL_092][サメの精霊]([x]1ターンの間、<b>隠れ身</b>。味方のミニオンの<b>雄叫び</b>と<b>コンボ</b>は2回発動する。)
-#[TRL_124][ぶんどり部隊](自分のデッキから海賊を2体引く。<b>コンボ:</b>さらに武器を1つ引く。)
-#[ULD_231][旋風脚流の達人]([x]自分が<b>コンボ</b>カードを手札から使用する度ランダムな<b>コンボ</b>カード1枚を自分の手札に追加する。)
-#スレッド 'MainThread' (0x1) はコード 0 (0x0) で終了しました。
-#[ULD_285][鉤付きシミター](<b>コンボ:</b>攻撃力+2を獲得する。)
-#[UNG_063][カミツキソウ](<b>コンボ:</b> このターン中で先に使用されたカード1枚につき+1/+1を獲得する。)
-#[UNG_064][ヴァイルスパイン・スレイヤー](<b>コンボ:</b>ミニオン1体を破壊する。)
-#[YOD_017][影の彫刻家]([x]<b>コンボ:</b>このターン中に先に使用されたカード1枚につきカードを1枚引く。)
-	elif '味方に海賊がいる' in dscrpt or '味方の海賊' in dscrpt:
-#[AT_032][闇商人]([x]<b>雄叫び:</b>味方に海賊がいる場合___+1/+1を獲得する。)
-#[AT_070][空賊船長クラッグ](<b>突撃ィィィィ</b>味方の海賊1体につきコストが（1）減る。)
-#[NEW1_027][南海の船長](自身を除く味方の海賊は+1/+1を得る。)
-#[TRL_071][ブラッドセイルの吠猿]([x]<b>急襲</b>、<b>雄叫び:</b>自身を除く味方の海賊1体につき+1/+1を獲得する。)
-#[TRL_127][大砲連射]([x]ランダムな敵1体に$3ダメージを与える。味方の海賊1体につき1回これを繰り返す。)
-		for card in player.characters:
-			if card.race == Race.PIRATE:
-				cond1 += 2
-
-#使わない
-#[BOT_447][結晶術師]([x]<b>雄叫び:</b>自分のヒーローに___5ダメージを与える。_____装甲を5獲得する。_)
-#[TRL_546][凶暴なリクガメ]([x]<b>雄叫び:</b>自分のヒーローに___5ダメージを与える。)
-#[UNG_087][ビタータイド・ヒドラ](このミニオンがダメージを受ける度自分のヒーローに___3ダメージを与える。)
-
+#自分が武器を装備する度
 
 #自分の手札にドラゴンがいる場合
 #自分のヒーローの体力を3以上回復する度
@@ -2865,6 +2775,19 @@ def RogueCat_Condition(game, card, target):
 #これのコピーに変身させる
 #自分のデッキに混ぜる
 
+def haveBeast(game):
+	player = game.current_player
+	for card in player.hand:
+		if card.race == Race.BEAST:
+			return True
+	return False
+
+def haveDeathrattle(game):
+	player = game.current_player
+	for card in player.hand:
+		if card.has_deathrattle:
+			return True
+	return False
 
 def haveDragon(game):
 	player = game.current_player
@@ -2873,16 +2796,85 @@ def haveDragon(game):
 			return True
 	return False
 
+def haveMinion(game):
+	player = game.current_player
+	for card in player.field:
+		if card.type == CardType.MINION:
+			return True
+	return False
+
+def havePirate(game):
+	player = game.current_player
+	for card in player.hand:
+		if card.race == Race.PIRATE:
+			return True
+	return False
+
+def haveWeapon(game):
+	player = game.current_player
+	for card in player.field:
+		if card.type == CardType.WEAPON:
+			return True
+	return False
+
 def noDeck(game):
-   player = game.current_player
-   return len(player.deck)==0
+	player = game.current_player
+	return len(player.deck)==0
 
 def noHand(game):
-   player = game.current_player
-   return len(player.hand)==0
+	player = game.current_player
+	return len(player.hand)==0
 
 def noCharacter(game):
    player = game.current_player
    return len(player.characters)==0
 
+def heHasMinion(game):
+	player = game.current_player.opponent
+	for card in player.field:
+		if card.type == CardType.MINION:
+			return True
+	return False
 
+def heHasDemon(game):
+	player = game.current_player.opponent
+	for card in player.field:
+		if card.race == Race.DEMON:
+			return True
+	return False
+
+def heHasDeathrattle(game):
+	player = game.current_player.opponent
+	for card in player.field:
+		if card.has_deathrattle:
+			return True
+	return False
+
+def heHasNonVanilla(game):
+	player = game.current_player.opponent
+	for card in player.field:
+		if len(card.data.descrption)>=3:
+			return True
+	return False
+
+def heHasTaunt(game):
+	player = game.current_player.opponent
+	for card in player.field:
+		if card.taunt:
+			return True
+	return False
+
+def haveNoDuplicate(game):
+	player = game.current_player
+	for card1 in player.field:
+		for card2 in player.field:
+			if card1!=card2 and card1.name==card2.name:
+				return True
+	return False
+
+def onPirate(game):
+	player = game.current_player
+	for card in player.field:
+		if card.race == Race.PIRATE:
+			return True
+	return False
